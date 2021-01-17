4c47062443c231c9d4c5bd72a822d16850987925
6mn12j <dbwk0404@hanmail.net>
2021-01-17 18:11:01 +0900

    [민주] 주식가격,프린터

--- programmers/스택,큐/프린터/printer_minju.js
+++ programmers/스택,큐/프린터/printer_minju.js
@@ -1,25 +1,21 @@
 function solution(priorities, location) {
     var answer = 0;
+    let cnt = 0 ;
     const priority = priorities.map((prior,index)=>({
-        target: index === location,
-        value : prior
+            index : index,
+            value : prior
     }));
-    /*
-    	[
-  { target: true, value: 1 },
-  { target: false, value: 1 },
- 
-    ]
-    */
-    while(true){
-        let current = priority.splice(0,1)[0];
-        if(priority.some(ing => ing.value > current.value)){
+    
+    while(priority.length>0){    
+        
+        const current = priority.shift();
+        if(priority.some(v => current.value < v.value)){
             priority.push(current);
         } else {
-            answer ++;
-            if(current.target) break;
+          answer++;
+          if(current.index === location) break;
         }
+
     }
-    console.log(priority);
     return answer;
 }
