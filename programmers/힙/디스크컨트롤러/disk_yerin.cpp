a0cda32c72384cf8c94ca29f6cd6b46001fcb1f5
Yellin36 <68840566+Yellin36@users.noreply.github.com>
2021-01-21 18:22:50 +0900

    Update disk_yerin.cpp

--- programmers/힙/디스크컨트롤러/disk_yerin.cpp
+++ programmers/힙/디스크컨트롤러/disk_yerin.cpp
@@ -4,36 +4,36 @@
 
 using namespace std;
 
-bool compare(vector<int> a, vector<int> b) {
-    return a[1] > b[1];
+bool compare(pair<int, int> a, pair<int, int> b) {
+    return a.second > b.second;
 }
-
 int solution(vector<vector<int>> jobs) {
-    int answer = 0, cur_time = 0, i = 0; 
+    int answer = 0, time = 0, i = 0;
     bool comp = false;
-    vector<vector<int>> q;
+    vector<pair<int, int>> q;
     
     sort(jobs.begin(), jobs.end());
 
-    while(i == jobs.size() && q.empty()) {
-        if(i < jobs.size() && jobs[i][0] <= cur_time) {
-            q.push_back(jobs[i++]);
-            comp = true;
+    while(i < jobs.size() || !q.empty()) {
+        if(i < jobs.size() && jobs[i][0] <= time) {
+            q.push_back({jobs[i][0], jobs[i][1]});
+            i++; comp = true;
             continue;
         }
-        if(q.empty()) {
-            cur_time = jobs[i][0];
-            q.push_back(jobs[i++]); // 정렬 횟수 줄이기
+        if(i < jobs.size() && q.empty()) {
+            q.push_back({jobs[i][0], jobs[i][1]});
+            time = jobs[i][0];
+            i++;
         }
         else {
             if(comp) {
                 sort(q.begin(), q.end(), compare); 
                 comp = false;
             }
-            cur_time += q.back()[1];
-            answer += cur_time - q.back()[0];
+            time += q.back().second;
+            answer += time - q.back().first;
             q.pop_back();
         }
     }
-    return answer / jobs.size();
+    return answer/jobs.size();
 }
