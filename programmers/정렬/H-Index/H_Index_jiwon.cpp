a8a68c584f6e3734167dd4071291c407b48253b4
Hanturtle <tjfdnjs0829@naver.com>
2021-01-31 18:46:10 +0900

    [지원] H-index

--- programmers/정렬/H-Index/H_Index_jiwon.cpp
+++ programmers/정렬/H-Index/H_Index_jiwon.cpp
@@ -6,18 +6,16 @@ using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
-    sort(citations.begin(), citations.end());
     
-    int h=0;
-    while(h<citations.back()){
-        int cnt=0;
-        for(int i=0;i<citations.size();i++){
-            if(citations.at(i)>=h)
-                cnt++;
-        }
-        if(cnt>=h)
-            answer = h;
-        h++;
+    sort(citations.begin(), citations.end(),greater<>());
+    vector<int> tmp;
+     for(int i=0;i<citations.size();i++){
+         tmp.push_back(min(citations[i],i+1));
     }
+    for(int i=0;i<tmp.size();i++){
+        if(tmp[i]>answer)
+            answer = tmp[i];
+    }
+    
     return answer;
 }
